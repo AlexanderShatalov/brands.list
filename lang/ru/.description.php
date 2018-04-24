0e6f3cd131dbb7deb02d6d27183c41e5e2bf73d6
Shata <nozomuaion@gmail.com>
2018-04-24 15:51:53 +0300

    copy to my namespace

---
+++ lang/ru/.description.php
@@ -0,0 +1,5 @@
+<?
+$MESS ['T_IBLOCK_DESC_LIST'] = "Список брендов";
+$MESS ['T_IBLOCK_DESC_LIST_DESC'] = "Список брендов из выгружаемых предложений";
+$MESS ['T_IBLOCK_DESC_NEWS'] = "Блок брендов";
+?>
