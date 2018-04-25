4daffef7935fe580b41ce586618410e92d6eebf2
Shata <nozomuaion@gmail.com>
2018-04-25 15:55:15 +0300

    delete templates excess

--- templates/shatalov_template/template.php
+++ templates/shatalov_template/template.php
@@ -16,34 +16,55 @@ $this->setFrameMode(true);
 <?if($arParams["DISPLAY_TOP_PAGER"]):?>
 	<?=$arResult["NAV_STRING"]?><br />
 <?endif;?>
-<table width="100%" cellspacing="0" cellpadding="5">
+<div id="table_brands">
 <?foreach($arResult["ITEMS"] as $arItem):?>
+    <?
+        /*echo '<pre>';
+        var_dump($arItem);
+        echo '</pre>';*/
+    ?>
 	<?
 	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
 	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
 	?>
 
-<tr id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-	<td valign="top" width="0%">
+	<table style="float:left; margin:25px;">
+	<tr>
+	<td>
+	<p class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
 		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
 			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img border="0" src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>" height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>" alt="<?=$arItem["NAME"]?>" hspace="0" vspace="3" title="<?=$arItem["NAME"]?>" style="float:left" /></a>
+				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img
+						class="preview_picture"
+						border="0"
+						src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+						width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
+						height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
+						alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+						title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+						style="float:left"
+						/></a>
 			<?else:?>
-				<img border="0" src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>" height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>" hspace="0" vspace="2" alt="<?=$arItem["NAME"]?>" title="<?=$arItem["NAME"]?>" style="float:left" />
+				<img
+					class="preview_picture"
+					border="0"
+					src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+					width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
+					height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
+					alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+					title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+					style="float:left"
+					/>
 			<?endif;?>
-		<?elseif(is_array($arItem["DISPLAY_PROPERTIES"]["DOC_TYPE"]) && file_exists($_SERVER['DOCUMENT_ROOT'].$templateFolder."/images/".strtolower($arItem["DISPLAY_PROPERTIES"]["DOC_TYPE"]["VALUE_XML_ID"]).".gif")):?>
-				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img border="0" src="<?=$templateFolder."/images/".strtolower($arItem["DISPLAY_PROPERTIES"]["DOC_TYPE"]["VALUE_XML_ID"]).".gif"?>" width="19" height="19" alt="<?=$arItem["DISPLAY_PROPERTIES"]["DOC_TYPE"]["VALUE_ENUM"]?>" hspace="0" vspace="3" title="<?=$arItem["DISPLAY_PROPERTIES"]["DOC_TYPE"]["VALUE_ENUM"]?>" style="float:left" /></a>
 		<?endif?>
-	</td>
-	<td valign="top" width="100%">
 		<?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
 			<span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
 		<?endif?>
 		<?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
 			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><?echo $arItem["NAME"]?></a><br />
+				<a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><b><?echo $arItem["NAME"]?></b></a><br />
 			<?else:?>
-				<?echo $arItem["NAME"]?><br />
+				<b><?echo $arItem["NAME"]?></b><br />
 			<?endif;?>
 		<?endif;?>
 		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
@@ -57,10 +78,7 @@ $this->setFrameMode(true);
 			<?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?>
 			</small><br />
 		<?endforeach;?>
-		<?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):
-			if($pid=="DOC_TYPE")
-				continue;
-			?>
+		<?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
 			<small>
 			<?=$arProperty["NAME"]?>:&nbsp;
 			<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
@@ -70,10 +88,11 @@ $this->setFrameMode(true);
 			<?endif?>
 			</small><br />
 		<?endforeach;?>
-	</td>
-</tr>
-<?endforeach;?>
+	</p>
+</td></tr>
 </table>
+<?endforeach;?>
+</div>
 <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
 	<br /><?=$arResult["NAV_STRING"]?>
 <?endif;?>
